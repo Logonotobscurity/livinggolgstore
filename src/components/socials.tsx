08fc2d245d34f561c4d402a55d5056075b3966f1
Rios Silvas <riosilvas827@gmail.com>
2025-09-15 16:37:48 +0000

    Ineed you to analyse the social media i need  it to match exact design s

--- src/components/socials.tsx
+++ src/components/socials.tsx
@@ -1,5 +1,17 @@
 import { Facebook, Twitter, Instagram } from 'lucide-react';
 
+function TikTokIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 288 512"
+      className="svg"
+    >
+      <path d="M288 152.2v50.93c-49.33-2.22-87-34.42-87-73.43 0-41.21 34.42-78.33 87-80.54V0C190.2.73 151.8 33.38 116.8 34.42 50.31 36.31 0 91.23 0 161.8c0 99.89 80.69 176.4 176.4 176.4 78.34 0 120.1-53.13 120.1-120.1-.81-8.82-1.9-17.4-3.5-25.9z" />
+    </svg>
+  );
+}
+
 export function Socials() {
   return (
     <div className="socials-card">
@@ -30,6 +42,15 @@ export function Socials() {
             <Instagram className="social-svg"/>
           </a>
           <div className="social-text">Instagram</div>
+        </li>
+        <li className="social-item">
+          <span></span>
+          <span></span>
+          <span></span>
+          <a href="#">
+            <TikTokIcon />
+          </a>
+          <div className="social-text">TikTok</div>
         </li>
       </ul>
     </div>
