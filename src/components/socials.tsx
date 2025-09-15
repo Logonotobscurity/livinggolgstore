d81ab36677506bdf2b4f5e03ca3bcb4cefd5de1e
Rios Silvas <riosilvas827@gmail.com>
2025-09-15 16:44:02 +0000

    Why cant it have same Like share so the best solution is just copy into

--- src/components/socials.tsx
+++ src/components/socials.tsx
@@ -5,7 +5,7 @@ function TikTokIcon() {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 288 512"
-      className="svg"
+      className="social-svg"
     >
       <path d="M288 152.2v50.93c-49.33-2.22-87-34.42-87-73.43 0-41.21 34.42-78.33 87-80.54V0C190.2.73 151.8 33.38 116.8 34.42 50.31 36.31 0 91.23 0 161.8c0 99.89 80.69 176.4 176.4 176.4 78.34 0 120.1-53.13 120.1-120.1-.81-8.82-1.9-17.4-3.5-25.9z" />
     </svg>
@@ -16,7 +16,7 @@ export function Socials() {
   return (
     <div className="socials-card">
       <ul>
-        <li className="social-item">
+        <li className="iso-pro">
           <span></span>
           <span></span>
           <span></span>
@@ -25,7 +25,7 @@ export function Socials() {
           </a>
           <div className="social-text">Facebook</div>
         </li>
-        <li className="social-item">
+        <li className="iso-pro">
           <span></span>
           <span></span>
           <span></span>
@@ -34,7 +34,7 @@ export function Socials() {
           </a>
           <div className="social-text">Twitter</div>
         </li>
-        <li className="social-item">
+        <li className="iso-pro">
           <span></span>
           <span></span>
           <span></span>
@@ -43,7 +43,7 @@ export function Socials() {
           </a>
           <div className="social-text">Instagram</div>
         </li>
-        <li className="social-item">
+        <li className="iso-pro">
           <span></span>
           <span></span>
           <span></span>
