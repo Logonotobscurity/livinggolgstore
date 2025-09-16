de4102cd88e023ee32d862ea46c765ef35b89c00
Rios Silvas <riosilvas827@gmail.com>
2025-09-16 03:37:32 +0000

    Replace twitter icon with svg from this template and use our existing co

--- src/components/socials.tsx
+++ src/components/socials.tsx
@@ -1,4 +1,4 @@
-import { Facebook, Twitter } from 'lucide-react';
+import { Facebook } from 'lucide-react';
 
 function InstagramIcon() {
   return (
@@ -18,6 +18,24 @@ function InstagramIcon() {
     </svg>
   );
 }
+
+function TwitterIcon(props: React.HTMLAttributes<SVGElement>) {
+  return (
+    <svg
+      {...props}
+      height="23"
+      viewBox="0 0 1200 1227"
+      width="23"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fill="currentColor"
+        d="M714.163 519.284L1160.89 0H1055.03L667.137 450.887L357.328 0H0L468.492 681.821L0 1226.37H105.866L515.491 750.218L842.672 1226.37H1200L714.137 519.284H714.163ZM569.165 687.828L521.697 619.934L144.011 79.6944H306.615L611.412 515.685L658.88 583.579L1055.08 1150.3H892.476L569.165 687.854V687.828Z"
+      />
+    </svg>
+  );
+}
+
 
 function TikTokIcon() {
   return (
@@ -49,7 +67,7 @@ export function Socials() {
           <span></span>
           <span></span>
           <a href="#">
-            <Twitter className="social-svg"/>
+            <TwitterIcon className="social-svg"/>
           </a>
           <div className="social-text">Twitter</div>
         </li>
