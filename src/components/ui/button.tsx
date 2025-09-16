00641488a0220855fbb24c0fcdef13382fd4ea09
Rios Silvas <riosilvas827@gmail.com>
2025-09-16 17:56:15 +0000

    replace with button structure : "const ButtonCrossArrow = () => {\n  ret

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { Slot } from '@radix-ui/react-slot';
 import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/lib/utils';
-import { ArrowRight } from 'lucide-react';
 
 const buttonVariants = cva(
   'button',
@@ -69,17 +68,47 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
 
     return (
       <button
-        className={cn(buttonVariants({ variant, size, className }))}
+        className={cn(buttonVariants({ variant, size, className }), 'group')}
         ref={ref}
         style={{ '--clr': color } as React.CSSProperties}
         {...props}
       >
         {children}
         {showIcon && size !== 'icon' && (
-          <span className="button__icon-wrapper">
-            <ArrowRight className="button__icon-svg" />
-            <ArrowRight className="button__icon-svg button__icon-svg--copy" />
-          </span>
+          <div className="button__icon-wrapper">
+             <div className="button__icon-svg-wrapper">
+                <svg
+                width='15'
+                height='15'
+                viewBox='0 0 15 15'
+                fill='none'
+                xmlns='http://www.w3.org/2000/svg'
+                className='button__icon-svg'
+                >
+                <path
+                    d='M3.64645 11.3536C3.45118 11.1583 3.45118 10.8417 3.64645 10.6465L10.2929 4L6 4C5.72386 4 5.5 3.77614 5.5 3.5C5.5 3.22386 5.72386 3 6 3L11.5 3C11.6326 3 11.7598 3.05268 11.8536 3.14645C11.9473 3.24022 12 3.36739 12 3.5L12 9.00001C12 9.27615 11.7761 9.50001 11.5 9.50001C11.2239 9.50001 11 9.27615 11 9.00001V4.70711L4.35355 11.3536C4.15829 11.5488 3.84171 11.5488 3.64645 11.3536Z'
+                    fillRule='evenodd'
+                    clipRule='evenodd'
+                    fill='currentColor'
+                ></path>
+                </svg>
+                <svg
+                width='15'
+                height='15'
+                viewBox='0 0 15 15'
+                fill='none'
+                xmlns='http://www.w3.org/2000/svg'
+                className='button__icon-svg button__icon-svg--copy'
+                >
+                <path
+                    d='M3.64645 11.3536C3.45118 11.1583 3.45118 10.8417 3.64645 10.6465L10.2929 4L6 4C5.72386 4 5.5 3.77614 5.5 3.5C5.5 3.22386 5.72386 3 6 3L11.5 3C11.6326 3 11.7598 3.05268 11.8536 3.14645C11.9473 3.24022 12 3.36739 12 3.5L12 9.00001C12 9.27615 11.7761 9.50001 11.5 9.50001C11.2239 9.50001 11 9.27615 11 9.00001V4.70711L4.35355 11.3536C4.15829 11.5488 3.84171 11.5488 3.64645 11.3536Z'
+                    fillRule='evenodd'
+                    clipRule='evenodd'
+                    fill='currentColor'
+                ></path>
+                </svg>
+            </div>
+          </div>
         )}
       </button>
     );
