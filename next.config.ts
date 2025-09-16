c8790c4e8a2ce631c04dfa6cbecdbb0f2e3ac59b
Rios Silvas <riosilvas827@gmail.com>
2025-09-16 01:39:41 +0000

    Implement updae of images not displaying and replace imaages that are bl

--- next.config.ts
+++ next.config.ts
@@ -52,6 +52,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'img.ydesigngroup.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
