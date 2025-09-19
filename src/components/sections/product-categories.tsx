e07fe12db85dc6b6b5b1682062a47d94b2cd0977
Rios Silvas <riosilvas827@gmail.com>
2025-09-19 19:29:24 +0000

    from dev component above  make it same background color with prodct page

--- src/components/sections/product-categories.tsx
+++ src/components/sections/product-categories.tsx
@@ -1,5 +1,6 @@
 import { PlaceHolderImages } from '@/lib/placeholder-images';
 import { CategoryCard } from '@/components/category-card';
+import { ReusableBackground } from '../reusable-background';
 
 export default function ProductCategories() {
   const productCategories = [
@@ -12,29 +13,31 @@ export default function ProductCategories() {
   const categories = PlaceHolderImages.filter(img => productCategories.includes(img.id));
 
   return (
-    <section className="py-20 md:py-32 bg-background">
-      <div className="container mx-auto px-6 max-w-7xl">
-        <div className="text-left mb-12 md:mb-20">
-          <h2 className="font-headline text-3xl md:text-5xl font-bold text-foreground">
-            Explore Our Products
-          </h2>
-          <p className="mt-4 text-lg text-muted-foreground">
-            Get Inspired - Living Gold Selections for you
-          </p>
+    <ReusableBackground>
+      <section className="py-20 md:py-32">
+        <div className="container mx-auto px-6 max-w-7xl">
+          <div className="text-left mb-12 md:mb-20">
+            <h2 className="font-headline text-3xl md:text-5xl font-bold text-foreground">
+              Explore Our Products
+            </h2>
+            <p className="mt-4 text-lg text-muted-foreground">
+              Get Inspired - Living Gold Selections for you
+            </p>
+          </div>
+          <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-x-4 md:gap-x-8 gap-y-8 md:gap-y-16 max-w-7xl mx-auto">
+            {categories.map((product, index) => (
+              <CategoryCard
+                key={product.id}
+                product={product}
+                animationDelay={`${index * 0.05}s`}
+                className="w-full"
+                imageClassName="w-full h-full p-2 sm:p-6"
+                priority={index < 2}
+              />
+            ))}
+          </div>
         </div>
-        <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-x-4 md:gap-x-8 gap-y-8 md:gap-y-16 max-w-7xl mx-auto">
-          {categories.map((product, index) => (
-            <CategoryCard
-              key={product.id}
-              product={product}
-              animationDelay={`${index * 0.05}s`}
-              className="w-full"
-              imageClassName="w-full h-full p-2 sm:p-6"
-              priority={index < 2}
-            />
-          ))}
-        </div>
-      </div>
-    </section>
+      </section>
+    </ReusableBackground>
   );
 }
