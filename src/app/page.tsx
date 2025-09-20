f16004242c9368794278f883d036370cc3dcaaf6
Rios Silvas <riosilvas827@gmail.com>
2025-09-20 01:58:40 +0000

    you can see a black theme on th white bg kindly delete to ensure reduce

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ import TradesAndServices from '@/components/sections/trades-and-services';
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen bg-black">
+    <div className="flex flex-col min-h-screen bg-background">
       <Header />
       <main className="flex-grow">
         <Hero />
